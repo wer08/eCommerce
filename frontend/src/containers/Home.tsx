d5d8cc836d7f7fa3f4ee23a059d1b1f9bf05a927
wer08 <wz08061992@gmail.com>
2023-03-27 16:09:38 +0200

    setting up redux toolkit

---
+++ frontend/src/containers/Home.tsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from '../assets/react.svg'
+import viteLogo from '/vite.svg'
+import '../App.css'
+
+import { useAppSelector, useAppDispatch } from '../../hooks'
+
+import { decrement, increment } from '../../features/counter/counterSlice'
+
+const Home = () => {
+    const count = useAppSelector((state) => state.counter.value)
+  const dispatch = useAppDispatch()
+
+  const onClick = () => {
+    dispatch(increment())
+  }
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => onClick()}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+ 
+export default Home;
