af20dd3b3a0477649509c7b48f4fe8965b488786
wer08 <wz08061992@gmail.com>
2023-03-28 13:05:46 +0200

    set up login page

--- frontend/src/containers/Home.tsx
+++ frontend/src/containers/Home.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link } from 'react-router-dom'
 import '../App.css'
 
 import { useAppSelector, useAppDispatch } from '../../hooks'
@@ -8,7 +6,7 @@ import { useAppSelector, useAppDispatch } from '../../hooks'
 import { decrement, increment } from '../../features/counter/counterSlice'
 
 const Home = () => {
-    const count = useAppSelector((state) => state.counter.value)
+  const count = useAppSelector((state) => state.counter.value)
   const dispatch = useAppDispatch()
 
   const onClick = () => {
@@ -17,26 +15,9 @@ const Home = () => {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => onClick()}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Link className='btn btn-primary' to = "/login">
+        Login
+      </Link>
     </div>
   )
 }
--- frontend/src/containers/Login.tsx
+++ frontend/src/containers/Login.tsx
@@ -1,5 +1,81 @@
+import { useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+
+
 const Login = () => {
-    return ( <></> );
-}
+    const [formData, setFormData] = useState({
+        login: "",
+        password: ""
+    })
+    const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+    const { login, password } = formData
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+        setFormData({...formData, [e.target.name]: e.target.value})
+    }
+
+    const onSubmit = (e:React.FormEvent<HTMLFormElement>)=>{
+
+        e.preventDefault();
+        console.log('login')
+
+    }
+
+    // const continueWithGoogle = async () => {
+    //     try{
+    //         const res = await axios.get(`${import.meta.env.VITE_API_URL}/auth/o/google-oauth2/?redirect_uri=http://localhost:5173/google`)
+    //         window.location.replace(res.data.authorization_url)
+    //     }catch(e){
+    //         console.log(e)
+    //     }
+    // }
+
+    // const continueWithFacebook = async () => {
+    //     try{
+    //         const res = await axios.get(`${import.meta.env.VITE_API_URL}/auth/o/facebook/?redirect_uri=http://localhost:5173/facebook`)
+    //         window.location.replace(res.data.authorization_url)
+    //     }catch(e){
+    //         console.log(e)
+    //     }
+    // }
+    //Is the User authenticated ?
+    //Naviagte to Home
+    if (isAuthenticated)
+    {
+        return <Navigate to='/'></Navigate>
+    }
+
+    return ( 
+        <div className="container mt-5">
+            <h1>Sign In</h1>
+            <p>Sign into you account</p>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <input className="form-control mb-2" type='email' placeholder="Email" name="email" value={login} onChange={e=>onChange(e)} required />
+                </div>
+                <div className="form-group">
+                    <input className="form-control mb-2" type='password' placeholder="Password" name="password" minLength={6} value={password} onChange={e=>onChange(e)} required />
+                </div>
+                <button className="btn btn-primary" type='submit'>Login</button>
+            </form>
+            {/* <div>
+                <button className="btn btn-danger mt-3" onClick={continueWithGoogle}>
+                    Continue with Google
+                </button>
+            </div>
+            <div>
+                <button className="btn btn-primary mt-3" onClick={continueWithFacebook}>
+                    Continue with Facebook
+                </button>
+            </div> */}
+            <p className="mt-3">
+                Don't have an account ? <Link to='/signup'>SignUp</Link>
+            </p>
+            <p className="mt-3">
+                Forgot your password ? <Link to='/reset_password'>Reset Password</Link>
+            </p>
+        </div>
+     );
+};
  
 export default Login;
