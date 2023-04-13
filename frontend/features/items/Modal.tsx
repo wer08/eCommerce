47525fc9a77764f8130fed0f1b4506e957e7df1e
wer08 <wz08061992@gmail.com>
2023-04-13 15:31:44 +0200

    added ability to add to a cart

--- frontend/features/items/Modal.tsx
+++ frontend/features/items/Modal.tsx
@@ -2,12 +2,25 @@ import React from "react";
 import { TItem } from "./types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faShoppingCart, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { useAppDispatch } from "../../hooks";
+import { addItem } from "../cart/cartSlice";
 
 interface Props {
   selectedItem: TItem;
 }
 
 const Modal: React.FC<Props> = ({ selectedItem }) => {
+
+
+    const dispatch = useAppDispatch()
+    const item = {...selectedItem, quantity: 1}
+
+    const addToCart = () => {
+        dispatch(addItem(item))
+    }
+
+
+
   return (
     <div className="modal fade" id="itemModal" tabIndex={-1} role="dialog" aria-labelledby="itemModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
@@ -22,7 +35,7 @@ const Modal: React.FC<Props> = ({ selectedItem }) => {
                 <h5 className="font-weight-bold">{selectedItem.name}</h5>
                 <p className="my-4">{selectedItem.description}</p>
                 <h4 className="mb-4">${selectedItem.price}</h4>
-                <button type="button" className="btn btn-primary w-100 mb-3">
+                <button type="button" className="btn btn-primary w-100 mb-3" onClick={addToCart}>
                   <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
                   Add to cart
                 </button>
