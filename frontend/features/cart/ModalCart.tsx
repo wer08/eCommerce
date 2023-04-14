e9897d0b56e4ed154cc19c547f861a92a3fe9590
wer08 <wz08061992@gmail.com>
2023-04-14 10:00:16 +0200

    Fixed Modal(added info when cart is empty)

--- frontend/features/cart/ModalCart.tsx
+++ frontend/features/cart/ModalCart.tsx
@@ -9,13 +9,14 @@ const ModalCart: React.FC = () => {
 
   const items = useAppSelector(selectItems)
 
+
   return (
     <div className="modal fade" id="cartModal" tabIndex={-1} role="dialog" aria-labelledby="cartModalLabel" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content">
 
           <div className="modal-body">
-            {items.map(item => (
+            {items ? items.map(item => (
               <div className="row mb-4 shadow">
                   <div className="col p-0">
                   <div className="card">
@@ -28,7 +29,14 @@ const ModalCart: React.FC = () => {
                   </div>
                   </div>
               </div>
-            ))}
+            )) : 
+            <div className="row mb-4 shadow">
+                <div className="col p-0">
+                <div className="card">
+                    <h1>No items in the cart</h1>
+                </div>
+                </div>
+            </div>}
           </div>
         </div>
       </div>
