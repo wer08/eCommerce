c3d0737e8d1529510fb17365a4eed760a508badd
Wojtek <wz08061992@gmail.com>
2023-05-05 15:29:15 +0200

    Wokinf on JWT verification, Added Role Enu, Added Jwt Authentication Filte, Jwt Service

---
+++ backend/backend/src/main/java/com/example/backend/configuration/JwtService.java
@@ -0,0 +1,39 @@
+package com.example.backend.configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET_KEY = "2948404D635166546A576E5A7234753778214125442A462D4A614E645267556B";
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+            .parserBuilder()
+            .setSigningKey(getSignInKey())
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+}
--- backend/backend/src/main/java/com/example/backend/model/Client.java
+++ backend/backend/src/main/java/com/example/backend/model/Client.java
@@ -3,20 +3,27 @@ package com.example.backend.model;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.List;
 
 import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.EnumType.STRING;
 import static jakarta.persistence.FetchType.EAGER;
 import static jakarta.persistence.GenerationType.AUTO;
 
 @Entity
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Client
+public class Client implements UserDetails
 {
     @Id
     @GeneratedValue(strategy = AUTO)
@@ -25,6 +32,10 @@ public class Client
     private String username;
     @Column(unique = true)
     private String email;
+
+    private String firstName;
+    private String lastName;
+    private String password;
     @OneToMany(mappedBy = "client", cascade = ALL)
     private List<Item> items;
 
@@ -33,5 +44,33 @@ public class Client
         this.id = id;
         this.username = username;
         this.email = email;
+    }
+
+    @Enumerated(STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
